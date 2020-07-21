7f1f67e4671d41381532ac6b61ea82879d129c14
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-07-21 13:21:07 +0530

    Update call_back_button_handler.py (#69)
    * Update call_back_button_handler.py
    
    * Update call_back_button_handler.py
    
    * Update call_back_button_handler.py
    
    * Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# (c) Shrimadhav U K | gautamajay52
 
 # the logging things
 import logging
@@ -15,26 +15,29 @@ from tobrot.helper_funcs.youtube_dl_button import youtube_dl_call_back
 from tobrot.plugins.status_message_fn import cancel_message_f
 
 async def button(bot, update: CallbackQuery):
-	cb_data = update.data
-	g = await AdminCheck(bot, update.message.chat.id, update.from_user.id)
-	print(g)
-	if (update.from_user.id == update.message.reply_to_message.from_user.id) or g:
-		print(cb_data)
-		if cb_data.startswith("cancel"):
-			if len(cb_data) > 1:
-				i_m_s_e_g = await update.message.reply_text("checking..?", quote=True)
-				aria_i_p = await aria_start()
-				g_id = cb_data.split()[-1]
-				LOGGER.info(g_id)
-				try:
-					downloads = aria_i_p.get_download(g_id)
-					LOGGER.info(downloads)
-					LOGGER.info(downloads.remove(force=True))
-					await i_m_s_e_g.edit_text(f"Leech Cancelled by <a href='tg://user?id={update.from_user.id}'>{update.from_user.first_name}</a>")
-				except Exception as e:
-					await i_m_s_e_g.edit_text("<i>FAILED</i>\n\n" + str(e) + "\n#error")
-			else:
-				await update.message.delete()
-	
-		elif "|" in cb_data:
-			await youtube_dl_call_back(bot, update)
+    cb_data = update.data
+    try:
+        g = await AdminCheck(bot, update.message.chat.id, update.from_user.id)
+        print(g)
+    except:
+        pass
+    if "|" in cb_data:
+        await youtube_dl_call_back(bot, update)
+    if (update.from_user.id == update.message.reply_to_message.from_user.id) or g:
+        print(cb_data)
+        if cb_data.startswith("cancel"):
+            if len(cb_data) > 1:
+                i_m_s_e_g = await update.message.reply_text("checking..?", quote=True)
+                aria_i_p = await aria_start()
+                g_id = cb_data.split()[-1]
+                LOGGER.info(g_id)
+                try:
+                    downloads = aria_i_p.get_download(g_id)
+                    LOGGER.info(downloads)
+                    LOGGER.info(downloads.remove(force=True))
+                    await i_m_s_e_g.edit_text(f"Leech Cancelled by <a href='tg://user?id={update.from_user.id}'>{update.from_user.first_name}</a>")
+                except Exception as e:
+                    await i_m_s_e_g.edit_text("<i>FAILED</i>\n\n" + str(e) + "\n#error")
+                else:
+                    await update.message.delete()
+				
