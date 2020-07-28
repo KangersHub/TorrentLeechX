552441f4c9bf00fb5505cea105df0a87cd95bae0
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-07-28 14:45:38 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -4,6 +4,8 @@
 
 # the logging things
 import logging
+import os
+import shutil
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 LOGGER = logging.getLogger(__name__)
@@ -13,7 +15,10 @@ from tobrot.helper_funcs.admin_check import AdminCheck
 from tobrot.helper_funcs.download_aria_p_n import aria_start
 from tobrot.helper_funcs.youtube_dl_button import youtube_dl_call_back
 from tobrot.plugins.status_message_fn import cancel_message_f
-
+from tobrot import (
+    MAX_MESSAGE_LENGTH,
+    AUTH_CHANNEL
+)
 async def button(bot, update: CallbackQuery):
     cb_data = update.data
     try:
@@ -23,7 +28,7 @@ async def button(bot, update: CallbackQuery):
         pass
     if "|" in cb_data:
         await youtube_dl_call_back(bot, update)
-    if (update.from_user.id == update.message.reply_to_message.from_user.id) or g:
+    elif (update.from_user.id == update.message.reply_to_message.from_user.id) or g:
         print(cb_data)
         if cb_data.startswith("cancel"):
             if len(cb_data) > 1:
@@ -40,4 +45,25 @@ async def button(bot, update: CallbackQuery):
                     await i_m_s_e_g.edit_text("<i>FAILED</i>\n\n" + str(e) + "\n#error")
                 else:
                     await update.message.delete()
+        elif cb_data == "fuckingdo":
+            if update.from_user.id in AUTH_CHANNEL:
+                g_d_list = ['app.json', 'venv', 'rclone.conf', '.gitignore', '_config.yml', 'COPYING', 'Dockerfile', 'DOWNLOADS', 'Procfile', 'rclone.jpg', 'README.md', 'requirements.txt', 'runtime.txt', 'start.sh', 'tobrot', 'vendor']
+                LOGGER.info(g_d_list)
+                g_list = os.listdir()
+                LOGGER.info(g_list)
+                g_del_list = list(set(g_list)-set(g_d_list))
+                LOGGER.info(g_del_list)
+                if len(g_del_list) != 0:
+                    for f in g_del_list:
+                        if os.path.isfile(f):
+                            os.remove(f)
+                        else:
+                            shutil.rmtree(f)
+                    await update.message.edit_text(f"Deleted {len(g_del_list)} objects 😬")
+                else:
+                    await update.message.edit_text("Nothing to clear 🙄")
+            else:
+                await update.message.edit_text("You are not allowed to do that 🤭")
+        elif cb_data == "fuckoff":
+            await update.message.edit_text("Okay! fine 🤬")
 				
