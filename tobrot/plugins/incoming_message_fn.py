0d1f0d475763c7948d24690a7e8aabb6425bd9a4
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-08-01 08:07:11 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -215,7 +215,7 @@ async def g_yt_playlist(client, message):
     i_m_sefg = await message.reply_text("Processing...you should wait🤗", quote=True)
     if len(message.command) > 1:
         if message.command[1] == "gdrive":
-            with open('blame_my_knowledge.txt', 'w+') as gg:
+            with open('blame_my_knowledge_again.txt', 'w+') as gg:
                 gg.write("I am noob and don't know what to do that's why I have did this")
     if 'www.youtube.com/playlist' in message.reply_to_message.text:
         await yt_playlist_downg(message.reply_to_message, i_m_sefg)
