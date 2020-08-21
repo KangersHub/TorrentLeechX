aa001c6b06ee9c311e152de0153631d19cb1843d
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-08-22 00:24:01 +0530

    Tried to remove remanants after cancellation of torrent 😶

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -38,13 +38,17 @@ async def button(bot, update: CallbackQuery):
                 LOGGER.info(g_id)
                 try:
                     downloads = aria_i_p.get_download(g_id)
+                    file_name = downloads.name
                     LOGGER.info(downloads)
                     LOGGER.info(downloads.remove(force=True))
+                    if os.path.exists(file_name):
+                        if os.path.isdir(file_name):
+                            shutil.rmtree(file_name)
+                        else:
+                            os.remove(file_name)
                     await i_m_s_e_g.edit_text(f"Leech Cancelled by <a href='tg://user?id={update.from_user.id}'>{update.from_user.first_name}</a>")
                 except Exception as e:
                     await i_m_s_e_g.edit_text("<i>FAILED</i>\n\n" + str(e) + "\n#error")
-                else:
-                    await update.message.delete()
         elif cb_data == "fuckingdo":
             if update.from_user.id in AUTH_CHANNEL:
                 g_d_list = ['app.json', 'venv', 'rclone.conf', '.gitignore', '_config.yml', 'COPYING', 'Dockerfile', 'DOWNLOADS', 'Procfile', '.heroku', '.profile.d', 'rclone.jpg', 'README.md', 'requirements.txt', 'runtime.txt', 'start.sh', 'tobrot', 'vendor']
