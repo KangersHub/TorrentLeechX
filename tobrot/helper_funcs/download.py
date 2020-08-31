203cd1a01b36b6fdd29988669c5e45325c8664b0
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-08-31 13:19:55 +0530

    😕

--- tobrot/helper_funcs/download.py
+++ tobrot/helper_funcs/download.py
@@ -15,6 +15,7 @@ LOGGER = logging.getLogger(__name__)
 import asyncio
 import math
 import os
+import re
 import time
 import subprocess
 from datetime import datetime
@@ -36,7 +37,7 @@ async def down_load_media_f(client, message):
         os.makedirs(DOWNLOAD_LOCATION)
     if message.reply_to_message is not None:
         start_t = datetime.now()
-        download_location = DOWNLOAD_LOCATION + "/"
+        download_location = "/app/"
         c_time = time.time()
         the_real_download_location = await client.download_media(
             message=message.reply_to_message,
@@ -48,12 +49,11 @@ async def down_load_media_f(client, message):
         )
         end_t = datetime.now()
         ms = (end_t - start_t).seconds
-        print(the_real_download_location)
+        LOGGER.info(the_real_download_location)
         await asyncio.sleep(10)
         await mess_age.edit_text(f"Downloaded to <code>{the_real_download_location}</code> in <u>{ms}</u> seconds")
-        gk = subprocess.Popen(['mv', f'{the_real_download_location}', '/app/'], stdout = subprocess.PIPE)
-        out = gk.communicate()
         the_real_download_location_g = os.path.basename(the_real_download_location)
+        LOGGER.info(the_real_download_location_g)
         if len(message.command) > 1:
             if message.command[1] == "unzip":
                 file_upload = await unzip_me(the_real_download_location_g)
