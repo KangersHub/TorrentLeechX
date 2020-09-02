1020af64437501abf6f07e15156c6ebcd44e7bab
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-09-02 22:45:05 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -53,7 +53,7 @@ async def button(bot, update: CallbackQuery):
                     await update.message.delete()
         elif cb_data == "fuckingdo":
             if update.from_user.id in AUTH_CHANNEL:
-                g_d_list = ['app.json', 'venv', 'rclone.conf', '.gitignore', '_config.yml', 'COPYING', 'Dockerfile', 'DOWNLOADS', 'Procfile', '.heroku', '.profile.d', 'rclone.jpg', 'README.md', 'requirements.txt', 'runtime.txt', 'start.sh', 'tobrot', 'gautam', 'Torrentleech-Gdrive.log', 'vendor']
+                g_d_list = ['app.json', 'venv', 'rclone.conf', '.gitignore', '_config.yml', 'COPYING', 'Dockerfile', 'DOWNLOADS', 'Procfile', '.heroku', '.profile.d', 'rclone.jpg', 'README.md', 'requirements.txt', 'runtime.txt', 'start.sh', 'tobrot', 'gautam', 'Torrentleech-Gdrive.txt', 'vendor']
                 LOGGER.info(g_d_list)
                 g_list = os.listdir()
                 LOGGER.info(g_list)
