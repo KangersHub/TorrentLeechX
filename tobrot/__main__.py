74bda7a73608a1a2996dd935f2bb1e093b6a9302
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-12-05 16:55:46 +0530

    ...o_u...

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -42,7 +42,7 @@ from tobrot import (
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 
-from tobrot.plugins.new_join_fn import new_join_f, help_message_f, rename_message_f
+from tobrot.plugins.new_join_fn import new_join_f, help_message_f
 from tobrot.plugins.incoming_message_fn import rename_tg_file, incoming_message_f, incoming_youtube_dl_f, incoming_purge_message_f, incoming_gdrive_message_f, g_yt_playlist, g_clonee
 from tobrot.plugins.rclone_size import check_size_g, g_clearme
 from tobrot.plugins.status_message_fn import (
