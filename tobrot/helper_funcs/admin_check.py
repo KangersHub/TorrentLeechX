1c59245e54c0811d6084ee2eb2c6894b17d7350e
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2021-01-25 16:35:15 +0530

    some fix may be -||-

--- tobrot/helper_funcs/admin_check.py
+++ tobrot/helper_funcs/admin_check.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from tobrot import AUTH_CHANNEL
 
 async def AdminCheck(client, chat_id, user_id):
+    chat = await client.get_chat(chat_id)
+    if chat.type == "private" and chat_id in AUTH_CHANNEL:
+        return True
     SELF = await client.get_chat_member(
         chat_id=chat_id,
         user_id=user_id
