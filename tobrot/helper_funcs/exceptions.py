ea35c3a1636f55bfd977823658a2ef65133c152a
Amirul Islam <amirulandalib@gmail.com>
2021-05-26 23:43:08 +0600

    added direct link generator+ exceptions
    added direct link generator+ exceptions

---
+++ tobrot/helper_funcs/exceptions.py
@@ -0,0 +1,8 @@
+class DirectDownloadLinkException(Exception):
+    """𝙉𝙤 𝙢𝙚𝙩𝙝𝙤𝙙 𝙛𝙤𝙪𝙣𝙙 𝙛𝙤𝙧 𝙚𝙭𝙩𝙧𝙖𝙘𝙩𝙞𝙣𝙜 𝙙𝙞𝙧𝙚𝙘𝙩 𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙 𝙡𝙞𝙣𝙠 𝙛𝙧𝙤𝙢 𝙩𝙝𝙚 𝙝𝙩𝙩𝙥 𝙡𝙞𝙣𝙠"""
+    pass
+
+
+class NotSupportedExtractionArchive(Exception):
+    """𝙏𝙝𝙚 𝙖𝙧𝙘𝙝𝙞𝙫𝙚 𝙛𝙤𝙧𝙢𝙖𝙩 𝙪𝙨𝙚 𝙞𝙨 𝙩𝙧𝙮𝙞𝙣𝙜 𝙩𝙤 𝙚𝙭𝙩𝙧𝙖𝙘𝙩 𝙞𝙨 𝙣𝙤𝙩 𝙨𝙪𝙥𝙥𝙤𝙧𝙩𝙚𝙙"""
+    pass
