8d8b4301743c7ccfbb63042d529367ecbd4039d2
Akshit <87356298+akshit-21@users.noreply.github.com>
2021-09-22 10:52:46 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -63,9 +63,9 @@ async def upload_to_tg(
 ):
     base_file_name = os.path.basename(local_file_name)
     caption_str = ""
-    caption_str += "<code>"
+    caption_str += ""
     caption_str += base_file_name
-    caption_str += "</code>"
+    caption_str += ""
     if os.path.isdir(local_file_name):
         directory_contents = os.listdir(local_file_name)
         directory_contents.sort()
