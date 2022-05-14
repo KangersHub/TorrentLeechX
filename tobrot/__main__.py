d17120ca0b4835fd3505893aa8381250d9126311
David Shadow <92687730+David-Shadow@users.noreply.github.com>
2022-05-15 01:36:41 +0530

    extra_fn

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -44,7 +44,10 @@ from tobrot import (
     TOGGLE_DOC,
     HELP_COMMAND,
     SPEEDTEST,
-    TSEARCH_COMMAND
+    TSEARCH_COMMAND,
+    MEDIAINFO_COMMAND,
+    MANNUAL_GUP_COMMAND,
+    TG_DL_COMMAND
 )
 from tobrot.helper_funcs.download import down_load_media_f
 from tobrot.plugins.call_back_button_handler import button
@@ -52,11 +55,15 @@ from tobrot.plugins.call_back_button_handler import button
 from tobrot.plugins.torrent_search import searchhelp
 from tobrot.plugins.choose_rclone_config import rclone_command_f
 from tobrot.plugins.custom_thumbnail import clear_thumb_nail, save_thumb_nail
-from tobrot.plugins.incoming_message_fn import (g_clonee, g_yt_playlist,
-                                                incoming_message_f,
-                                                incoming_purge_message_f,
-                                                incoming_youtube_dl_f,
-                                                rename_tg_file)
+from tobrot.plugins.incoming_message_fn import (
+    g_clonee,
+    g_yt_playlist,
+    incoming_message_f,
+    incoming_purge_message_f,
+    incoming_youtube_dl_f,
+    rename_tg_file
+)
+from tobrot.plugins.extra_fn import mannual_gd_upload, link_mediainfo_fn, dl_to_local_fn
 from tobrot.plugins.new_join_fn import help_message_f, new_join_f
 from tobrot.plugins.speedtest import get_speed
 from tobrot.plugins.rclone_size import check_size_g, g_clearme
