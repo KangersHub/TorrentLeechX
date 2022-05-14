e047dcb91b287408051f27875ccc64fa500db6e1
David Shadow <92687730+David-Shadow@users.noreply.github.com>
2022-05-15 01:46:59 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -79,29 +79,31 @@ from tobrot.plugins.status_message_fn import (
 )
 '''
 botcmds = [
-        (f'{BotCommands.ExtractCommand}', 'Unarchive items and upload to telegram'),
-        (f'{BotCommands.gExtractCommand}','This will unarchive file and upload to cloud.'),
-        (f'{BotCommands.ArchiveCommand}','Archive as .tar.gz acrhive and upload to Telegram'),
-        (f'{BotCommands.gArchiveCommand}','rchive as .tar.gz acrhive and upload to Cloud'),
-        (f'{BotCommands.telegramfleechCommand}','This will mirror the telegram files to ur respective cloud .'),
-        (f'{BotCommands.telegramexleechCommand}','This will unarchive telegram file and upload to cloud.'),
-        (f'{BotCommands.gcloneCommand}','This command is used to clone gdrive files or folder using gclone'),
-        (f'{BotCommands.YtdlCommand}','upload yt-dlp supported video links to Telegram'),
-        (f'{BotCommands.gYtdlCommand}','upload yt-dlp supported video links to Cloud'),
-        (f'{BotCommands.PytdlCommand}','upload yt-dlp supported playlist links to Telegram'),
-        (f'{BotCommands.gPytdlCommand}','upload yt-dlp supported playlist links to Cloud'),
-        (f'{BotCommands.ToggleDocCommand}','choose whether the file shall be uploaded as doc or not'),
-        (f'{BotCommands.ToggleVidCommand}','choose whether the file shall be uploaded as streamable or not'),
-        (f'{BotCommands.SaveCommand}','Save Thumbnail For Telegram Uploads'),
-        (f'{BotCommands.ClearCommand}','Clear Thumbnail to default For Telegram Uploads'),
-        (f'{BotCommands.RenameCommand}','Rename Telegram File and reupload it telegram'),
-        (f'{BotCommands.StatusCommand}','Show Bot stats and concurrent Downloads'),
-        (f'{BotCommands.SpeedCommand}','Get Speedtest of the Host'),
-        (f'{BotCommands.HelpCommand}','Get Help'),
-        (f'{BotCommands.LogCommand}','Get the Bot Log [Owner Only]'),
-        (f'{BotCommands.TsHelpCommand}','Get help for Torrent Search Module')
-
-    ]
+    (f'{BotCommands.ExtractCommand}', 'Unarchive items and upload to telegram'),
+    (f'{BotCommands.gExtractCommand}','This will unarchive file and upload to cloud.'),
+    (f'{BotCommands.ArchiveCommand}','Archive as .tar.gz acrhive and upload to Telegram'),
+    (f'{BotCommands.gArchiveCommand}','rchive as .tar.gz acrhive and upload to Cloud'),
+    (f'{BotCommands.telegramfleechCommand}','This will mirror the telegram files to ur respective cloud .'),
+    (f'{BotCommands.telegramexleechCommand}','This will unarchive telegram file and upload to cloud.'),
+    (f'{BotCommands.gcloneCommand}','This command is used to clone gdrive files or folder using gclone'),
+    (f'{BotCommands.YtdlCommand}','upload yt-dlp supported video links to Telegram'),
+    (f'{BotCommands.gYtdlCommand}','upload yt-dlp supported video links to Cloud'),
+    (f'{BotCommands.PytdlCommand}','upload yt-dlp supported playlist links to Telegram'),
+    (f'{BotCommands.gPytdlCommand}','upload yt-dlp supported playlist links to Cloud'),
+    (f'{BotCommands.ToggleDocCommand}','choose whether the file shall be uploaded as doc or not'),
+    (f'{BotCommands.ToggleVidCommand}','choose whether the file shall be uploaded as streamable or not'),
+    (f'{BotCommands.SaveCommand}','Save Thumbnail For Telegram Uploads'),
+    (f'{BotCommands.ClearCommand}','Clear Thumbnail to default For Telegram Uploads'),
+    (f'{BotCommands.RenameCommand}','Rename Telegram File and reupload it telegram'),
+    (f'{BotCommands.StatusCommand}','Show Bot stats and concurrent Downloads'),
+    (f'{BotCommands.SpeedCommand}','Get Speedtest of the Host'),
+    (f'{BotCommands.HelpCommand}','Get Help'),
+    (f'{BotCommands.LogCommand}','Get the Bot Log [Owner Only]'),
+    (f'{BotCommands.TsHelpCommand}','Get help for Torrent Search Module'),
+    (f'{BotCommands.TgdlCommand}',"Download tg file to bot's local storage"),
+    (f'{BotCommands.MediainfoCommand}','Get Mediainfo from link'),
+    (f'{BotCommands.MannualgUpCommand}',"Upload a file from bot's local server to gdrive")
+]
 '''
 if __name__ == "__main__":
     # create download directory, if not exist
@@ -283,8 +285,25 @@ if __name__ == "__main__":
         & filters.chat(chats=AUTH_CHANNEL),
     )
     app.add_handler(searchhelp_handler)
-    #
+    # Download a Telegram file to bot's local storage
+    dl_to_local_handler = MessageHandler(
+        dl_to_local_fn,
+        filters=filters.command([TG_DL_COMMAND, f"{TG_DL_COMMAND}@{bot.username}"]) & filters.chat(AUTH_CHANNEL),
+    )
+    app.add_handler(dl_to_local_handler)
+    # Manually upload a file to gdrive by giving it's path
+    mannual_gd_upload_handler = MessageHandler(
+        mannual_gd_upload,
+        filters=filters.command([f"{MANNUAL_GUP_COMMAND}", f"{MANNUAL_GUP_COMMAND}@{bot.username}"]) & filters.chat(AUTH_CHANNEL),
+    )
+    app.add_handler(mannual_gd_upload_handler)
+    # Get mediainfo by link
+    mediainfo_handler = MessageHandler(
+        link_mediainfo_fn,
+        filters = filters.command([MEDIAINFO_COMMAND, f'{MEDIAINFO_COMMAND}@{bot.username}']) & filters.chat(AUTH_CHANNEL),
+    )
+    app.add_handler(mediainfo_handler)
 
 app.start()
-logging.info(f"@{(app.get_me()).username} Has Started Running...🏃💨💨")
+logging.info(f"@{bot.username} Has Started Running...🏃💨💨")
 idle()
